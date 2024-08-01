347b23ee7f4dcde8bfa65498e298f5a073ac9972
Kelvin Mock <kelvin.mock@gmail.com>
2024-08-01 11:20:36 -0400

    Fixed issue to access .env in Typescript
    Related issue: Issue #9
    https://github.com/kmock930/CICS-Hackathon-Team-1/issues/9

---
+++ backend/global.d.ts
@@ -0,0 +1,10 @@
+namespace NodeJS {
+    interface ProcessEnv {
+        SUPABASE_PWD: string;
+        API_KEY: string;
+        API_SECRET: string;
+        API_URL: string;
+        API_ANON: string;
+        PORT: number;
+    }
+  }
