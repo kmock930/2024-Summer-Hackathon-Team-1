b2af9954c776dfa0dfa52a1607b5dd3a75c41691
Ualnomis <ualnamnuhc@gmail.com>
2024-07-31 22:45:31 -0400

    feat: select component

---
+++ frontend/src/components/Select/Select.tsx
@@ -0,0 +1,106 @@
+import * as React from 'react';
+import * as RadixSelect from '@radix-ui/react-select';
+import {
+  CheckIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+} from '@radix-ui/react-icons';
+import { css } from '@pigment-css/react';
+
+interface SelectOptionProps {
+  value: string;
+  text: string;
+}
+
+function Select({ options }: { options: SelectOptionProps[] }) {
+  return (
+    <RadixSelect.Root>
+      <RadixSelect.Trigger
+        className={css({
+          display: 'inline-flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: '4px',
+          padding: '0 15px',
+          fontSize: '13px',
+          lineHeight: 1,
+          height: '35px',
+          gap: '5px',
+          backgroundColor: 'white',
+          color: 'var(--violet-11)',
+          boxShadow: '0 2px 10px var(--black-a7)',
+        })}
+      >
+        <RadixSelect.Value placeholder='Select a Before and/or After Camp Option' />
+        <RadixSelect.Icon />
+      </RadixSelect.Trigger>
+
+      <RadixSelect.Portal>
+        <RadixSelect.Content
+          className={css({
+            overflow: 'hidden',
+            backgroundColor: 'white',
+            borderRadius: '6px',
+            boxShadow:
+              '0px 10px 38px -10px rgba(22, 23, 24, 0.35), 0px 10px 20px -15px rgba(22, 23, 24, 0.2)',
+          })}
+        >
+          <RadixSelect.ScrollUpButton />
+          <RadixSelect.Viewport>
+            {options.map((option) => {
+              return (
+                <SelectItem value={option.value} key={option.value}>
+                  {option.text}
+                </SelectItem>
+              );
+            })}
+          </RadixSelect.Viewport>
+          <RadixSelect.ScrollDownButton />
+          <RadixSelect.Arrow />
+        </RadixSelect.Content>
+      </RadixSelect.Portal>
+    </RadixSelect.Root>
+  );
+}
+
+const SelectItem = React.forwardRef<
+  HTMLDivElement,
+  { children: React.ReactNode; value: string }
+>(({ children, ...props }, forwardedRef) => {
+  return (
+    <RadixSelect.Item
+      {...props}
+      ref={forwardedRef}
+      className={css({
+        fontSize: '13px',
+        lineHeight: 1,
+        color: 'var(--violet-11)',
+        borderRadius: '3px',
+        display: 'flex',
+        alignItems: 'center',
+        height: '25px',
+        padding: '0 35px 0 25px',
+        position: 'relative',
+        userSelect: 'none',
+      })}
+    >
+      <RadixSelect.ItemText>{children}</RadixSelect.ItemText>
+      <RadixSelect.ItemIndicator
+        className={css({
+          position: 'absolute',
+          left: '0',
+          width: '25px',
+          display: 'inline-flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        })}
+      >
+        <CheckIcon />
+      </RadixSelect.ItemIndicator>
+    </RadixSelect.Item>
+  );
+});
+
+SelectItem.displayName = 'SelectItem';
+
+export default Select;
